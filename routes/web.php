2a4b9d4da7d9b78ca5ddd7bea5dfd1bf309095ad
erico <a31089@aluno.filipa-vilhena.edu.pt>
2022-12-12 12:51:25 +0000

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/parceiros', function () {
+    return view('parceiros');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
